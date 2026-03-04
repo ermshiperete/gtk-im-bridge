7ced796270d186e3c16a3e0ed7de3e64c9faabca
Eberhard Beilharz <eb1@sil.org>
2026-03-04 20:25:25 +0100

    chore: commit workspace changes from IM module work

--- src/module.c
+++ src/module.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include <gmodule.h>
+#include <gio/gio.h>
 #include "im-bridge.h"
 #include "logging.h"
 #include "ibus-setup.h"
@@ -39,4 +40,42 @@ im_module_create(const char *context_id)
   
   LOG_EXIT("im_module_create", "context=%p", (void *)context);
   return context;
+}
+
+/* GIO-style module entry points expected by GTK's module loader. */
+
+G_MODULE_EXPORT void
+g_io_module_load(GIOModule *module)
+{
+  g_type_module_use(G_TYPE_MODULE(module));
+
+  /* If this were a G_DEFINE_DYNAMIC_TYPE we'd call the generated
+   * register function here (e.g. gtk_im_bridge_context_register_type
+   * (G_TYPE_MODULE (module))). We still ensure the type is known by
+   * calling the get_type helper and initialize module state.
+   */
+  gtk_im_bridge_context_get_type();
+
+  /* Register the extension point implementation: name, required type,
+   * extension name, priority. */
+  g_io_extension_point_implement("gtk-im-module",
+                                GTK_TYPE_IM_CONTEXT,
+                                "im-bridge",
+                                10);
+
+  im_module_init(G_TYPE_MODULE(module));
+}
+
+G_MODULE_EXPORT void
+g_io_module_unload(GIOModule *module)
+{
+  im_module_exit();
+  g_type_module_unuse(G_TYPE_MODULE(module));
+}
+
+G_MODULE_EXPORT char **
+g_io_module_query(void)
+{
+  static char *eps[] = { "gtk-im-module", NULL };
+  return g_strdupv(eps);
 }
